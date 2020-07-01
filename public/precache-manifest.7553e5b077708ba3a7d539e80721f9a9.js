eacc7e45f006b970ac840f031bb5162238f96dc5
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-07-01 20:23:49 +0200

    f

--- public/precache-manifest.7553e5b077708ba3a7d539e80721f9a9.js
+++ public/precache-manifest.7553e5b077708ba3a7d539e80721f9a9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b0b0deb4e2f56f3f8d9f10a9acf6b9f8",
+    "revision": "8df377ee0dbd1ef0c0ad149b22ac055a",
     "url": "/index.html"
   },
   {
-    "revision": "465feffce11b552452b5",
-    "url": "/static/js/2.8e17d8ba.chunk.js"
+    "revision": "7e5d58a909f3396288da",
+    "url": "/static/js/2.90f1288d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.8e17d8ba.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.90f1288d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "06f2897ef7bcc5c13e20",
-    "url": "/static/js/main.2e34e830.chunk.js"
+    "revision": "c1b191a50a390983a3f0",
+    "url": "/static/js/main.61fff12d.chunk.js"
   },
   {
     "revision": "39a7be925db58886ddaf",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.837dcb3771a0d418d047e8c305f471be.js"
+  "/precache-manifest.7553e5b077708ba3a7d539e80721f9a9.js"
 );
 
 self.addEventListener('message', (event) => {
