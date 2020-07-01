87a8c91e9f0a50054f386ab86920763363f7e347
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-07-01 14:47:40 +0200

    heroku deployemnt

---
+++ public/precache-manifest.bcd77e79635c37d295d71bb426c3fd3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5aefa22a70f94850fa11183f7c3321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465feffce11b552452b5",
+    "url": "/static/js/2.8e17d8ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e17d8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7e08ae6fda31c18411",
+    "url": "/static/js/main.90a97da9.chunk.js"
+  },
+  {
+    "revision": "39a7be925db58886ddaf",
+    "url": "/static/js/runtime-main.a25c0300.js"
+  }
+]);
